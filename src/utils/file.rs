29bb36a2fa6666d6df2c16ecad0e8d9fdcb0da4b
Toby Bridle <foreignspaghettibolognese@gmail.com>
2022-03-21 14:23:56 +0000

    State of Filter is now Persistent (FIX)

--- src/utils/file.rs
+++ src/utils/file.rs
@@ -32,9 +32,9 @@ pub mod utils
          if _line.starts_with("Filter: ")
          {
             if &_line[8..] == "Other" { controller.set_state(TodoState::Other); }
-            if &_line[8..] == "NotDone" { controller.set_state(TodoState::NotDone); }
-            if &_line[8..] == "InProgress" { controller.set_state(TodoState::InProgress); }
-            if &_line[8..] == "Done" { controller.set_state(TodoState::Done); }
+            else if &_line[8..] == "NotDone" { controller.set_state(TodoState::NotDone); }
+            else if &_line[8..] == "InProgress" { controller.set_state(TodoState::InProgress); }
+            else &_line[8..] == "Done" { controller.set_state(TodoState::Done); }
          } else
          {
          todo_container.add_todo(_Todo { id: todo_container.todos.len() as i32, content: _line[active.to_string().len()+2..].to_string(), active });
